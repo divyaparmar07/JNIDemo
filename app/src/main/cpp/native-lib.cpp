a56c2ffcd16534cdd8acbd37927c8a8a71282ceb
Divya Parmar <divyaparmar0207@gmail.com>
2024-03-07 16:04:32 +0530

    Initial Commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,24 +1,40 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_jnidemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
+//extern "C" JNIEXPORT jstring JNICALL
+//Java_com_example_jnidemo_MainActivity_stringFromJNI(
+//        JNIEnv* env,
+//        jobject /* this */) {
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+//}
 
 extern "C"
 JNIEXPORT jint JNICALL
 Java_com_example_jnidemo_MainActivity_additionFromJNI(JNIEnv *env, jobject thiz, jint num1,
                                                       jint num2) {
-//    std::int64_t res = num1+num2;
+
     return num1 + num2;
 }
-//extern "C"
-//JNIEXPORT jint JNICALL
-//Java_com_example_jnidemo_MainActivity_additionFromJNI(JNIEnv *env, jobject thiz, jint num1,
-//                                                      jint num2) {
-//    return env->;
-//}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_jnidemo_MainActivity_subtractionFromJNI(JNIEnv *env, jobject thiz, jint num1,
+                                                         jint num2) {
+    return num1 - num2;
+}
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_jnidemo_MainActivity_sumOfArrayFromJNI(JNIEnv *env, jobject thiz,
+                                                        jintArray int_array) {
+    jint *arr = env->GetIntArrayElements(int_array, nullptr);
+    jint res = 0;
+    jint size = env->GetArrayLength(int_array);
+    for (int i = 0; i < size; ++i) {
+        res += arr[i];
+    }
+    env->ReleaseIntArrayElements(int_array,arr,NULL);
+    __android_log_write(ANDROID_LOG_DEBUG,"LOG_FROM_JNI_FUNCTION","Sum Of Array");
+    return res;
+}
--- app/src/main/java/com/example/jnidemo/MainActivity.java
+++ app/src/main/java/com/example/jnidemo/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.jnidemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jnidemo.databinding.ActivityMainBinding;
 
@@ -18,13 +19,11 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("jnidemo");
     }
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.jnidemo.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Example of a call to a native method
@@ -33,28 +32,59 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editText1 = binding.editTextNumber1;
         EditText editText2 = binding.editTextNumber2;
+        Button buttonAdd = binding.buttonSubmitAdd;
+        Button buttonSub = binding.buttonSubmitSub;
+        Button buttonSumOfArray = binding.buttonSumOfArray;
 
-        Button button = binding.buttonSubmitAdd;
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
 
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
                 try {
                     String str1 = editText1.getText().toString();
                     String str2 = editText2.getText().toString();
-                    if (str1 != null && str1.length() >0 && str2 != null && str2.length() >0 ) {
+                    if (str1.length() > 0 && str2.length() > 0) {
                         int num1 = Integer.parseInt(editText1.getText().toString());
                         int num2 = Integer.parseInt(editText2.getText().toString());
-//                Log.d("ans", "" + additionFromJNI(10, 20));
                         int ans = additionFromJNI(num1, num2);
-//                int ans = num1+num2;
                         tv.setText("Addition is: " + ans);
                     } else {
-                        // LOG
+                        Toast.makeText(getApplicationContext(), "Enter number1 and number2", Toast.LENGTH_SHORT).show();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                try {
+                    String str1 = editText1.getText().toString();
+                    String str2 = editText2.getText().toString();
+                    if (str1.length() > 0 && str2.length() > 0) {
+                        int num1 = Integer.parseInt(editText1.getText().toString());
+                        int num2 = Integer.parseInt(editText2.getText().toString());
+                        int ans = subtractionFromJNI(num1, num2);
+                        tv.setText("Subtraction is: " + ans);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Enter number1 and number2", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        buttonSumOfArray.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                int ans = sumOfArrayFromJNI(new int[]{10, 20, 30, 40, 50});
+                tv.setText("Sum Of Array is:" + ans);
             }
         });
 
@@ -64,7 +94,10 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'jnidemo' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
-
+//    public native String stringFromJNI();
     public native int additionFromJNI(int num1, int num2);
+
+    public native int subtractionFromJNI(int num1, int num2);
+
+    public native int sumOfArrayFromJNI(int[] intArray);
 }
